02fa45ab540be659dc8735955797ac5ae954ec76
Wolvye <alainsoltau@gmail.com>
2024-10-08 17:14:45 +0200

    Push
    lesson

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        int i3 =berechnen(2,4);
-        int i5 =berechnen(i3, 25);
-       berechnen(i5,-5);
-        wort();
 
-    }
-    public static void konsolenAusgabe(boolean b){
-        if (b){
-            System.out.println("Der Wert ist wahr!");
+    public static void main(String[] args){
+        //Einlesen von Konsole
+        Scanner scanner = new Scanner(System.in);
 
-        }else{
-            System.out.println("Der Wert ist Falsch!");
-        }
-    }
-    public static void ersteMethode() {
-        System.out.println("Hallo Welt!");
-    }
-    public static void neueMethode(int a){
-        System.out.println(a);
-    }
-    public static void mehrereArgumente(float f, boolean b){
-        if (b){
-            System.out.println(f);
-        }
+        System.out.println("Bitte gib den Rechnungsbetrag ein");
+        String eingabeRechnung = scanner.nextLine();
 
-    }
-    public static void dreiArgumente(int a, int b ,boolean c){
-        if (c){
-            System.out.println(a+b);
-        }else {
-            System.out.println(a-b);
-        }
+        System.out.println("Wieviel Prozent Trinkgeld möchtest du geben (zb.10%)?:");
+        String eingabeProzent =scanner.nextLine();
 
-    }
-    public static void erstetzen(int a, int b){
-        int c = a+b;
-        System.out.println(c);
-    }
+        System.out.println("Weiviele Personen waren beteiligt?:");
+        String eingabePersonen = scanner.nextLine();
 
-    public static int subtrahieren(int a, int b){
-        int c = a-b;
-        return c;
-    }
-    public static int berechnen(int a, int b){
-        int c=a+b;
-        System.out.println(c);
-        return c;
-    }
-    public static void wort(){
-        int i =200;
-        String zeichenkette ="Alles klar," + i;
-        System.out.println(zeichenkette + " Herr Kommissar?"+ (i+i));
-        float f =2.5f;
-        int i2 = (int) f;
-        System.out.println(i2); // die 0.5 geht verloren. Das liegt am casting von float zu int
+        //Verarbeiten von Eingaben
+        float rechnung = Float.parseFloat(eingabeRechnung);
+        int prozent = Integer.parseInt(eingabeProzent);
+        int personen = Integer.parseInt(eingabePersonen);
 
-        String s = "2.5";
-        //int i3 = Integer.parseInt(s);
-        float f3 = Float.parseFloat(s);
-    }
+        //Berechnen von Daten
+        float trinkgeld =rechnung *(prozent /100f);
+        float rechnungGes = rechnung + trinkgeld;
 
+
+        //Ausgeben von Daten
+        System.out.println("Ihr möchtet " + trinkgeld + " Trinkgeld geben.");
+        System.out.println("Somit zahlt ihr " +  rechnungGes + ".");
+    }
 
 }
 
